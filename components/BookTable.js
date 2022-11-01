10cfc945a0398f3ea5023f36052c0a14aed3d0d6
“Matt <matt.s.demartino@gmail.com>
2022-11-01 15:07:56 -0400

    Create book table

--- components/BookTable.js
+++ components/BookTable.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function BookTable(props) {
+  const bookData = props.bookList.map((book) => (
+    <tr key={book.id}>
+      <td>{book.name}</td>
+      <td>{book.author}</td>
+      <td>{book.genre}</td>
+      <td>{book.status}</td>
+      <td>{book.rating}</td>
+    </tr>
+  ));
   return (
-    <div></div>
-  )
+    <table>
+      <thead>
+        <tr>
+          <th>Book Title</th>
+          <th>Author</th>
+          <th>Genre</th>
+          <th>Status</th>
+          <th>Rating</th>
+        </tr>
+      </thead>
+      <tbody>
+        {bookData}
+      </tbody>
+    </table>
+  );
 }
