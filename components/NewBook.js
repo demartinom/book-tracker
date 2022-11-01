14e0ed6863d75744ebef1d1e061b944dd827d2ba
“Matt <matt.s.demartino@gmail.com>
2022-11-01 12:37:29 -0400

    Create initial book add form

--- components/NewBook.js
+++ components/NewBook.js
@@ -1,17 +1,38 @@
 import React from "react";
-import { BookForm } from "../styles/NewBook.styled";
+import { BookForm, FormElement } from "../styles/NewBook.styled";
 
 export default function NewBook() {
   return (
     <BookForm action="">
-      <input type="text" placeholder="Book Name" />
-      <label>
-        Date Started
-        <input type="date" name="Date Started" id="Start" />
-      </label>
-      <label>
-        Date Finished <input type="date" name="Date Finished" id="Finish" />
-      </label>
+      <FormElement>
+        <label htmlFor="name">Book Name</label>
+        <input type="text" id="name" />
+      </FormElement>
+      <FormElement>
+        <label htmlFor="author">Author</label>
+        <input type="text" id="author" />
+      </FormElement>
+      <FormElement>
+        <label htmlFor="genre">Genre</label>
+        <input type="text" id="genre" />
+      </FormElement>
+      <FormElement>
+        <label htmlFor="bookStatus">Book Status</label>
+        <select name="Book Status" id="bookStatus">
+          <option value="" disabled selected>
+            Choose an Option
+          </option>
+          <option value="finished">Finished</option>
+          <option value="reading">Currently Reading</option>
+          <option value="want">Want to Read</option>
+        </select>
+      </FormElement>
+      <FormElement>
+        {" "}
+        <label htmlFor="rating">Book Rating</label>
+        {/* Rating Placeholder */}
+        <input type="text" id="rating" />
+      </FormElement>
       <button>Add Book</button>
     </BookForm>
   );
