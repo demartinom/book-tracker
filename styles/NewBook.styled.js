cc06d5c98072159c03b4c0c45507879ff9b9fc9c
“Matt <matt.s.demartino@gmail.com>
2022-11-01 14:28:19 -0400

    Remove button styling

--- styles/NewBook.styled.js
+++ styles/NewBook.styled.js
@@ -3,9 +3,6 @@ import styled from "styled-components";
 export const BookForm = styled.form`
   display: flex;
   align-items: center;
-  button {
-    align-self: flex-end;
-  }
 `;
 
 export const FormElement = styled.div`
