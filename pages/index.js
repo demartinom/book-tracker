324159c9c47917b5b6604ccd00d5372eff1b69a7
“Matt <matt.s.demartino@gmail.com>
2022-11-01 14:49:03 -0400

    Remove book mapping from index

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,9 @@ import React from "react";
 
 export default function Home() {
   const [bookList, setBookList] = React.useState([]);
-  const bookTable = bookList.map((book) => (
-    <div key={book.id}>
-      <h1>{book.name}</h1>
-      <h2>{book.author}</h2>
-      <h3>{book.genre}</h3>
-      <h3>{book.status}</h3>
-      <h3>{book.rating}</h3>
-    </div>
-  ));
   return (
     <div>
       <NewBook setBooks={setBookList} />
-      {bookTable}
     </div>
   );
 }
