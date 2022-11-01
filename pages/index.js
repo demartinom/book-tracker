a3292d7000e4fb6b0cd4ba71c632ae90fda20bc3
“Matt <matt.s.demartino@gmail.com>
2022-11-01 13:14:01 -0400

    Change key from book name to book id

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Home() {
   const [bookList, setBookList] = React.useState([]);
   const bookTable = bookList.map((book) => (
-    <div key={book.name}>
+    <div key={book.id}>
       <h1>{book.name}</h1>
       <h2>{book.author}</h2>
       <h3>{book.genre}</h3>
