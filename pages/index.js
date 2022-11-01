97303cc5cbb848b4a62c4febc0a74952b376e821
“Matt <matt.s.demartino@gmail.com>
2022-11-01 13:04:38 -0400

    Add book mapping

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import NewBook from "../components/NewBook";
+import React from "react";
 
 export default function Home() {
   const [bookList, setBookList] = React.useState([]);
+  const bookTable = bookList.map((book) => (
+    <div key={book.name}>
+      <h1>{book.name}</h1>
+      <h2>{book.author}</h2>
+      <h3>{book.genre}</h3>
+      <h3>{book.status}</h3>
+      <h3>{book.rating}</h3>
+    </div>
+  ));
   return (
     <div>
-      <NewBook />
+      <NewBook setBooks={setBookList} />
+      {bookTable}
     </div>
   );
 }
