d1fbfff12eba45c99b7f6ca8c57ff2b7a0debb72
“Matt <matt.s.demartino@gmail.com>
2022-11-01 13:04:30 -0400

    Add book object and data functions

--- components/NewBook.js
+++ components/NewBook.js
@@ -1,27 +1,43 @@
 import React from "react";
 import { BookForm, FormElement } from "../styles/NewBook.styled";
 
-export default function NewBook() {
+export default function NewBook(props) {
+  const [bookData, setBookData] = React.useState({
+    name: "",
+    author: "",
+    genre: "",
+    status: "",
+    rating: 0,
+  });
+  function updateBookInfo(event) {
+    setBookData((prevData) => ({
+      ...prevData,
+      [event.target.name]: event.target.value,
+    }));
+    console.log(bookData);
+  }
+  function newBook(event) {
+    event.preventDefault();
+    props.setBooks((prevArray) => [...prevArray, bookData]);
+  }
   return (
     <BookForm action="">
       <FormElement>
         <label htmlFor="name">Book Name</label>
-        <input type="text" id="name" />
+        <input onChange={updateBookInfo} type="text" id="name" />
       </FormElement>
       <FormElement>
         <label htmlFor="author">Author</label>
-        <input type="text" id="author" />
+        <input onChange={updateBookInfo} type="text" id="author" />
       </FormElement>
       <FormElement>
         <label htmlFor="genre">Genre</label>
-        <input type="text" id="genre" />
+        <input onChange={updateBookInfo} type="text" id="genre" />
       </FormElement>
       <FormElement>
         <label htmlFor="bookStatus">Book Status</label>
         <select name="Book Status" id="bookStatus">
-          <option value="" disabled selected>
-            Choose an Option
-          </option>
+          <option defaultValue={"Choose an Option"} disabled>Choose an Option</option>
           <option value="finished">Finished</option>
           <option value="reading">Currently Reading</option>
           <option value="want">Want to Read</option>
@@ -31,9 +47,9 @@ export default function NewBook() {
         {" "}
         <label htmlFor="rating">Book Rating</label>
         {/* Rating Placeholder */}
-        <input type="text" id="rating" />
+        <input onChange={updateBookInfo} type="text" id="rating" />
       </FormElement>
-      <button>Add Book</button>
+      <button onClick={newBook}>Add Book</button>
     </BookForm>
   );
 }
