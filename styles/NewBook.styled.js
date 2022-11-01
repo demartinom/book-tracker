ada130acec5122564562e59cf76163bcd56bdf28
“Matt <matt.s.demartino@gmail.com>
2022-11-01 12:37:40 -0400

    Add form and form element styling

--- styles/NewBook.styled.js
+++ styles/NewBook.styled.js
@@ -2,5 +2,14 @@ import styled from "styled-components";
 
 export const BookForm = styled.form`
   display: flex;
+  align-items: center;
+  button {
+    align-self: flex-end;
+  }
+`;
+
+export const FormElement = styled.div`
+  display: flex;
   flex-direction: column;
+  align-items: center;
 `;
