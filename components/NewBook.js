a04ef2a208c2f263b34e392a67adf2dfd46a0171
“Matt <matt.s.demartino@gmail.com>
2022-11-01 12:15:36 -0400

    Begin creating NewBook form

--- components/NewBook.js
+++ components/NewBook.js
@@ -3,8 +3,16 @@ import { BookForm } from "../styles/NewBook.styled";
 
 export default function NewBook() {
   return (
-    <form action="">
-     
-    </form>
+    <BookForm action="">
+      <input type="text" placeholder="Book Name" />
+      <label>
+        Date Started
+        <input type="date" name="Date Started" id="Start" />
+      </label>
+      <label>
+        Date Finished <input type="date" name="Date Finished" id="Finish" />
+      </label>
+      <button>Add Book</button>
+    </BookForm>
   );
 }
