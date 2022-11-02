cb823b0e37beb6b5bf4ca22a09163c11ace49366
“Matt <matt.s.demartino@gmail.com>
2022-11-02 14:21:47 -0400

    Add stars visual to table book

--- components/BookTable.js
+++ components/BookTable.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { BookTableStyled } from "../styles/BookTable.styled";
 import { AiOutlineDelete } from "react-icons/ai";
+import dynamic from "next/dynamic";
+
+const RatingFunction = dynamic(
+  () => import("react-simple-star-rating").then((mod) => mod.Rating),
+  { ssr: false }
+);
 
 export default function BookTable(props) {
   function deleteBook(e) {
@@ -16,7 +22,9 @@ export default function BookTable(props) {
       <td>{book.author}</td>
       <td>{book.genre}</td>
       <td>{book.status}</td>
-      <td>{book.rating}</td>
+      <td>
+        <RatingFunction initialValue={book.rating} size={24} readonly={true}/>
+      </td>
       <td>
         <AiOutlineDelete onClick={deleteBook} />
       </td>
