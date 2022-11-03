9d3c778bf0a2403addbebc5087167e6b8e67fc3b
“Matt <matt.s.demartino@gmail.com>
2022-11-03 15:07:31 -0400

    Add update form

--- components/UpdateBook.js
+++ components/UpdateBook.js
@@ -1,7 +1,70 @@
-import React from 'react'
+import React from "react";
+import { BookForm, FormElement } from "../styles/BookForm";
+import dynamic from "next/dynamic";
 
-export default function UpdateBook() {
+const RatingFunction = dynamic(
+  () => import("react-simple-star-rating").then((mod) => mod.Rating),
+  { ssr: false }
+);
+export default function UpdateBook(props) {
+  function updateBookInfo(event) {
+    setBookData((prevData) => ({
+      ...prevData,
+      [event.target.name]: event.target.value,
+    }));
+  }
+  function updateRating(event) {
+    setBookData((prevData) => ({ ...prevData, rating: event }));
+  }
+  function newBook(event) {
+    event.preventDefault();
+    setBookData((prevData) => ({
+      ...prevData,
+      id: Math.floor(Math.random() * 1000),
+    }));
+    props.setBooks((prevArray) => [...prevArray, bookData]);
+  }
   return (
-    <div></div>
-  )
+    <BookForm action="">
+      <FormElement>
+        <label htmlFor="name">Book Name</label>
+        <input
+          onChange={updateBookInfo}
+          type="text"
+          name="name"
+          value={props.currentBook.name}
+        />
+      </FormElement>
+      <FormElement>
+        <label htmlFor="author">Author</label>
+        <input onChange={updateBookInfo} type="text" name="author"
+        value={props.currentBook.author} />
+      </FormElement>
+      <FormElement>
+        <label htmlFor="genre">Genre</label>
+        <input onChange={updateBookInfo} type="text" name="genre"
+        value={props.currentBook.genre} />
+      </FormElement>
+      <FormElement>
+        <label htmlFor="bookStatus">Book Status</label>
+        <select
+          name="status"
+          onChange={updateBookInfo}
+          defaultValue={props.currentBook.status}
+        >
+          <option value="default" disabled name="choose">
+            Choose an Option
+          </option>
+          <option value="Finished">Finished</option>
+          <option value="Currently Reading">Currently Reading</option>
+          <option value="Want to Read">Want to Read</option>
+        </select>
+      </FormElement>
+      <FormElement>
+        <label htmlFor="rating">Book Rating</label>
+        <RatingFunction size={24} onClick={updateRating} initialValue={props.currentBook.rating}/>
+      </FormElement>
+      <button onClick={newBook}>Add Book</button>
+    </BookForm>
+  );
 }
