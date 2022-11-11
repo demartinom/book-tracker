c072a84db4c0d5764c77dafbbff1b6b9eb607680
“Matt <matt.s.demartino@gmail.com>
2022-11-11 14:58:24 -0500

    Add gap between form items and font size

--- styles/BookForm.styled.js
+++ styles/BookForm.styled.js
@@ -4,13 +4,16 @@ export const BookForm = styled.form`
   display: flex;
   align-items: center;
   font-family: var(--font-playfair);
+  gap: .5rem;
   button {
     margin-top: 0.8rem;
   }
 `;
-
 export const FormElement = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  label {
+    font-size: 1.5rem;
+  }
 `;
