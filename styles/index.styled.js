d6253a002c5829486d93ece69f7cffd3d933fd89
“Matt <matt.s.demartino@gmail.com>
2022-11-11 12:31:40 -0500

    Darken screen when update is visible

--- styles/index.styled.js
+++ styles/index.styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const UpdateContainer = styled.div`
   position: absolute;
+  box-shadow: 0 0 0 1600px rgba(0, 0, 0, 0.65);
 `;
 export const TrackerContainer = styled.div`
   position: relative;
