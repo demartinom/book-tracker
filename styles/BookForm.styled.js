ff48b71584caf34df5ec589e7a0598e7e2838f49
“Matt <matt.s.demartino@gmail.com>
2022-11-11 14:48:25 -0500

    import Playfair font

--- styles/BookForm.styled.js
+++ styles/BookForm.styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const BookForm = styled.form`
   display: flex;
   align-items: center;
+  font-family: var(--font-playfair);
 `;
 
 export const FormElement = styled.div`
--- styles/sharedstyles.styled.js
+++ styles/sharedstyles.styled.js
@@ -2,8 +2,12 @@ import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-*{
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&display=swap');*{
     box-sizing: border-box;
     margin: 0;
-    padding: 0;}
+    padding: 0;
+    };
+    :root{
+        --font-playfair: 'Playfair Display', serif;
+    }
 `;
