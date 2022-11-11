adb1b1fbd7eca0cad099ff8072cf9fbcad1fa8dd
“Matt <matt.s.demartino@gmail.com>
2022-11-11 12:33:57 -0500

    Add positioning for update div

--- styles/index.styled.js
+++ styles/index.styled.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const UpdateContainer = styled.div`
   position: absolute;
   box-shadow: 0 0 0 1600px rgba(0, 0, 0, 0.65);
+  top: 15rem;
+  padding: 5rem;
 `;
 export const TrackerContainer = styled.div`
   position: relative;
