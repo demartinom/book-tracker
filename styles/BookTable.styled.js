2a400788b9bd7613c33b0713eb5ca19710b27734
“Matt <matt.s.demartino@gmail.com>
2022-11-14 15:34:59 -0500

    Add coloring to table

--- styles/BookTable.styled.js
+++ styles/BookTable.styled.js
@@ -6,21 +6,30 @@ export const BookTableStyled = styled.table`
   margin-left: 9rem;
   th,
   td {
-    width:12rem;
+    width: 12rem;
     font-size: 1.5rem;
   }
-  th{
+  th {
     background-color: var(--color-header);
   }
-  th:last-child{
+  th:last-child {
+    background-color: var(--color-background);
+  }
+  tr:nth-child(odd) {
+    background-color: var(--color-row-odd);
+  }
+  tr:nth-child(even) {
+    background-color: var(--color-row-even);
+  }
+  td:nth-child(6) {
     background-color: var(--color-background);
   }
 `;
 
 export const Actions = styled.td`
   display: flex;
-  gap: .5rem;
-  svg{
+  gap: 0.5rem;
+  svg {
     cursor: pointer;
   }
 `;
