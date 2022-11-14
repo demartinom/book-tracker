a485dd6595eef761484234f34b775821ec437119
“Matt <matt.s.demartino@gmail.com>
2022-11-14 12:38:57 -0500

    Creation of grid layout

--- styles/BookForm.styled.js
+++ styles/BookForm.styled.js
@@ -1,19 +1,40 @@
 import styled from "styled-components";
 
 export const BookForm = styled.form`
-  display: flex;
-  align-items: center;
-  font-family: var(--font-playfair);
-  gap: .5rem;
-  button {
-    margin-top: 0.8rem;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  .name-label {
+    grid-area: 1 / 1 / 2 / 2;
   }
-`;
-export const FormElement = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  label {
-    font-size: 1.5rem;
+  .name-input {
+    grid-area: 2 / 1 / 3 / 2;
+  }
+  .author-label {
+    grid-area: 1 / 2 / 2 / 3;
+  }
+  .author-input {
+    grid-area: 2 / 2 / 3 / 3;
+  }
+  .genre-label {
+    grid-area: 1 / 3 / 2 / 4;
+  }
+  .genre-input {
+    grid-area: 2 / 3 / 3 / 4;
+  }
+  .status-label {
+    grid-area: 1 / 4 / 2 / 5;
+  }
+  .status-input {
+    grid-area: 2 / 4 / 3 / 5;
+  }
+  .rating-label {
+    grid-area: 1 / 5 / 2 / 6;
+  }
+  .rating-input {
+    grid-area: 2 / 5 / 3 / 6;
+  }
+  .submit {
+    grid-area: 2 / 6 / 3 / 7;
   }
 `;
