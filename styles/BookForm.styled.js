269180fdb6e643999da572484e856c277389eeee
“Matt <matt.s.demartino@gmail.com>
2022-11-14 12:49:33 -0500

    Center labels over inputs

--- styles/BookForm.styled.js
+++ styles/BookForm.styled.js
@@ -4,6 +4,7 @@ export const BookForm = styled.form`
   display: grid;
   grid-template-columns: repeat(6, 1fr);
   grid-template-rows: repeat(2, 1fr);
+  text-align: center;
   .name-label {
     grid-area: 1 / 1 / 2 / 2;
   }
