5ef355ce5faac76e7c41da38ca338e2751a51d58
“Matt <matt.s.demartino@gmail.com>
2022-11-15 12:49:11 -0500

    Remove font import from styled components

--- styles/sharedstyles.styled.js
+++ styles/sharedstyles.styled.js
@@ -1,14 +1,13 @@
-import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&display=swap');*{
+    *{
     box-sizing: border-box;
     margin: 0;
     padding: 0;
     };
     :root{
-        --font-playfair: 'Playfair Display', serif;
+        --font-playfair: 'Kreon', serif;
         --color-background: #FAEDCD; 
         --color-font: #0C1618;
         --color-header: #D4A373;
