15852201486e36e0ccb4761e4f6cb816121e160a
“Matt <matt.s.demartino@gmail.com>
2022-12-21 13:30:08 -0500

    Create more responsive styling

--- styles/Hero.styled.js
+++ styles/Hero.styled.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const HeroStyled = styled.div`
   font-family: var(--font-rasa);
-  height: 5rem;
-  padding: 3rem;
+  padding: 1rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  text-align: center;
   background-color: var(--color-header);
 `;
