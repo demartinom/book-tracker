af78bb5e47a2c515fe3889993ba1875b480a28cb
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-04-24 14:04:28 -0400

    Revert "Main"

--- styles/mediaQueries.js
+++
@@ -1,7 +0,0 @@
-const sizes = {
-  mobile: "@media (max-width: 425px) ",
-  tablet: "@media (min-width: 426px) and (max-width: 768px) ",
-  computer: "@media (min-width: 769px)",
-};
-
-export default sizes;
